8a92f69c5759d6432a6479545101aaa58046ef15
batsieba <70596018+batsieba@users.noreply.github.com>
2023-07-14 22:22:17 +0400

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-
-/*
- *main - main block
- *Description: Print the alphabet on a line omitting e and q
- *Return 0
- *
+/**
+ * main - main block
+ * Description: Print the alphabet on a line omitting e and q
+ * using putchar only twice
+ * Return: 0
  */
-
 int main(void)
 {
-
-	char alpha = 'a';
-	while (alpha <= 'z')  
-	{
-		if (alpha != 'e' && alpha != 'q')
-			putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
-
-	return (0);
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+if (alpha == 'e' || alpha == 'q')
+alpha++;
+}
+putchar('\n');
+return (0);
 }
 
