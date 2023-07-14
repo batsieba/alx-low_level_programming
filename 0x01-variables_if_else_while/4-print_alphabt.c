005d759b5eec26a0d83b6efc9410e4875f036569
batsieba <batutekle8@gmail.com>
2023-07-14 11:13:08 -0700

    file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ *main - main block
+ *Write a program that prints the alphabet in lowercase, followed by a new line.
+ *Return 0
+ *
+ */
+
+int main(void)
+{
+
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		if( alpha != 'e' || alpha != 'q')
+			putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return(0);
+}
+
