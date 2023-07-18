f3a99c5f0fafb0907c439363df7aea4799ea106a
batsieba <70596018+batsieba@users.noreply.github.com>
2023-07-18 22:50:38 +0400

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
-
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
-	char word[]="_putchar";
-	int i;
-	for(i=0;i<sizeof(word); i++)
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(word[i]);
+		_putchar(abe[c]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
