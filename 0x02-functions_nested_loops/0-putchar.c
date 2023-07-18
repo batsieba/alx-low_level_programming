46ae580f49f4cad49b6125f1faef005eaab718ea
batsieba <batutekle8@gmail.com>
2023-07-18 11:49:16 -0700

    file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	char word[]="_putchar";
+	int i;
+	for(i=0;i<sizeof(word); i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar("\n");
 	return (0);
 }
