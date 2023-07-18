adcbeed4dddc431006c4f225c2f65efee48b66b2
batsieba <batutekle8@gmail.com>
2023-07-18 12:17:19 -0700

    file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * print_alphabet - check the code for ALX School students.
+ * Return -0
+ *
+ * */
+
+void print_alphabet(void);
+
+print_alphabet(void)
+{
+	char alpha[]="abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for(i = 0 ; i < sizeof(alpha) ; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar('\n');
+}
