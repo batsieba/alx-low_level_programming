30b3caa3c74d6c38b3f75007e86c3441dbd002f7
batsieba <batutekle8@gmail.com>
2023-07-21 03:47:15 -0700

    file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,8 @@
 /**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
-	 *
-	 * _isupper :  checks if letter is alphabet
-	 * @c - parameter to check
-	 * Return -  returns 1 is alphabet is capital or 0 if not
-	 * .
-	 */
+ * _isupper - checks for uppercase.
+ * @c: character to check.
+ * Return: 1 if c is uppercase, otherwise return 0.
+ */
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
