4a1c94290d3dac91a0a098e172284d1bc76baeeb
batsieba <batisebatekle@gmail.com>
2023-09-13 21:37:44 +0400

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,12 +11,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 unsigned int i;
-if (size <= 0)
-{
-/* code */
-return (-1);
-}
-if (array == NULL || cmp == NULL)
+if (array == NULL || size <= 0 || cmp == NULL)
 {
 return (-1);
 }
