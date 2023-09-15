02a71063fa6138eb46e36c6ab50b876ea66bfd26
batsieba <batisebatekle@gmail.com>
2023-09-15 17:11:03 +0400

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,16 +3,14 @@
 /**
  * sum_them_all - returns sum of all parameters
  * @n: arguments passed
+ * @...: a variable number of parameters
  * Return: int sum of arguments
+ * if n==0 -0
 */
 int sum_them_all(const unsigned int n, ...)
 {
-int i, sum = 0;
+unsigned int i, sum = 0;
 va_list ap;
-if (n == 0)
-{
-return (0);
-}
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
