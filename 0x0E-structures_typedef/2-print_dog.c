7241f45813facf8f2a1846a9e06016b56bbbe313
batsieba <batisebatekle@gmail.com>
2023-11-07 23:45:02 +0400

    betty style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-
-
 /**
 * print_dog - prints a struct dog
 * @d: pointer to structure
@@ -10,16 +8,16 @@
 */
 void print_dog(struct dog *d)
 {
-   if (d != NULL)
-   {
-       if (d->name != NULL)
-           printf("Name: %s\n", d->name);
-       else
-           printf("Name: (nil)\n");
-       printf("Age: %.6f\n", d->age);
-       if (d->owner != NULL)
-           printf("Owner: %s\n", d->owner);
-       else
-           printf("Owner: (nil)\n");
-   }
+if (d != NULL)
+{
+if (d->name != NULL)
+printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
+printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include "dog.h"
-
-
 /**
 * free_dog - frees dogs
 * @d: pointer to dog to free
@@ -10,11 +8,11 @@
 */
 void free_dog(dog_t *d)
 {
-   if (d)
-   {
-       free(d->name);
-       free(d->owner);
-       free(d);
-   }
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
 
